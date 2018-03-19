4ffcc192ce70b85851fa8d28b6d959442c98f12e
kosciej <kd305513@gmail.com>
2018-03-19 23:55:07 +0100

    flat improved

--- currency_downloader.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# author: Krystian Dowolski (krystian.dowolski@dealavo.com)
-
-import urllib2
-
-if __name__ == "__main__":
-    a = urllib2.urlopen('http://api.fixer.io/latest').read()
-    print(a)
--- flat/finder.py
+++ flat/finder.py
@@ -1,27 +1,32 @@
-from abc import abstractmethod
-import urllib2
-import os
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
 import ConfigParser
-from bs4 import BeautifulSoup
 import smtplib
+import sys
 import time
+import urllib2
+from abc import abstractmethod
+from urlparse import urlparse
 
 from lxml import etree
 
-from urlparse import urlparse
-
+reload(sys)
+sys.setdefaultencoding('utf8')
 htmlparser = etree.HTMLParser()
+
 
 class Rule(object):
 
     def __init__(self, xpath):
         self.xpath = xpath
 
-    def to_int(s):
+    @classmethod
+    def to_int(cls, s):
         res = '0'
         for c in s:
             if c.isdigit():
-                res +=c
+                res += c
         return int(res)
 
     @abstractmethod
@@ -37,10 +42,8 @@ class WordRule(Rule):
 
     def check(self, tree):
         offer_word = tree.xpath(self.xpath)[0].lower()
-        print(offer_word)
         for word in [w.lower() for w in self.words]:
             if word in offer_word:
-                print('work')
                 return True
         return False
 
@@ -53,55 +56,96 @@ class NumberRule(Rule):
         self.upper = upper
 
     def check(self, tree):
-        offer_number = self.to_int(tree.xpath(self.xpath)[0])
-        if offer_number <= upper and offer_number >= lower:
+        tmp = tree.xpath(self.xpath)
+        if not tmp: return False
+        offer_number = Rule.to_int(tree.xpath(self.xpath)[0])
+        if self.upper >= offer_number and self.lower <= offer_number:
             return True
         return False
 
-class Finder(object):
 
+class STMPWrapper(object):
 
-    def __init__(self, cfg):
-        conf = ConfigParser.RawConfigParser()
-        conf.read(cfg)
-        self.from_config(conf)
-        self.domain = '{uri.scheme}://{uri.netloc}'.format(uri=urlparse(self.url))
-        self.tree = etree.parse(urllib2.urlopen(self.url), htmlparser)
-        self.processed = []
-
-    def from_config(self, conf):
-        self.url = conf.get('general', 'url')
-        self.offers = conf.get('general', 'offers')
+    def __init__(self, conf):
+        """
+        @type conf: ConfigParser
+        """
+        self.conf = conf
+        self.smtp = self.conf.get('smtp', 'smtp')
+        self.port = int(self.conf.get('smtp', 'port'))
         self.sender = conf.get('smtp', 'from')
         self.rec = conf.get('smtp', 'to')
+        self.username = conf.get('smtp', 'username')
+        self.password = conf.get('smtp', 'password')
+        self.connected = False
+
+    def connect(self):
+        if not self.connected:
+            self.server = smtplib.SMTP("{}:{}".format(self.smtp, self.port))
+            self.server.ehlo()
+            self.server.starttls()
+            self.connected = True
+
+    def login(self):
+        self.server.login(self.username, self.password)
+
+    def sendmail(self, content):
+        message = """From: {}\nTo: {}\nSubject: {}\n\n{}""".format(
+            self.sender, self.rec, "MIESZKANIA", content)
+        if content:
+            self.server.sendmail(self.sender, [self.rec], message)
+
+
+class Finder(object):
+
+    def __init__(self, cfg_name):
+        """
+        @type cfg_name: str
+        """
+        self.conf = ConfigParser.RawConfigParser()
+        self.conf.read(cfg_name)
+        self.from_config(self.conf)
+        self.domain = '{uri.scheme}://{uri.netloc}'.format(uri=urlparse(self.url))
+        self.tree = etree.parse(urllib2.urlopen(self.url), htmlparser)
+        self.processed_urls = self.get_processed_urls()
+        self.connect_smtp()
+
+    def from_config(self, conf):
+        """
+        @type conf: ConfigParser
+        """
+        self.url = conf.get('general', 'url')
+        self.offers = conf.get('general', 'offers')
         self.parse_rules(conf)
-        
+
+    def get_processed_urls(self):
+        self.processed_urls_file = open("processed_urls", "a+")
+        return self.processed_urls_file.read().split('\n')
+
+    def connect_smtp(self):
+        self.smtp_wrapper = STMPWrapper(self.conf)
+        self.smtp_wrapper.connect()
+        self.smtp_wrapper.login()
+
     def parse_rules(self, conf):
         self.rules = []
         for section in conf.sections():
             if section.startswith('rule'):
                 if conf.has_option(section, 'word'):
-                    self.rules.append(WordRule(conf.get('rule1', 'xpath'), conf.get('rule1', 'word').split(',')))
+                    self.rules.append(WordRule(
+                        conf.get(section, 'xpath'), conf.get(section, 'word').split(',')))
                 else:
                     lower = 0
                     upper = 100000
                     if conf.has_option(section, 'upper'):
-                        upper = conf.get(section, 'upper')
+                        upper = int(conf.get(section, 'upper'))
                     if conf.has_option(section, 'lower'):
-                        lower = conf.get(section, 'lower')
-                    self.rules.append(Number(conf.get('rule1', 'xpath'), lower, upper))
+                        lower = int(conf.get(section, 'lower'))
+                    self.rules.append(NumberRule(conf.get(section, 'xpath'), lower, upper))
 
-
-    def send_email(self, content):
-        print(content)
-        if not content:
-            return
-        try:
-            smtpObj = smtplib.SMTP('localhost')
-            smtpObj.sendmail(self.sender, [self.rec], content)
-        except:
-            pass
-
+    def sendmail(self, content):
+        print "sending email with %d offers" % len(content.splitlines())
+        self.smtp_wrapper.sendmail(content)
 
     def run(self):
         while True:
@@ -109,9 +153,11 @@ class Finder(object):
             content = ''
             for href in hrefs:
                 full_url = self.domain + href
-                if full_url in self.processed:
-                    break
-                self.processed.append(full_url)
+                if full_url in self.processed_urls:
+                    continue
+                print "processing url %s" % full_url
+                self.processed_urls.append(full_url)
+                self.processed_urls_file.write(full_url + '\n')
                 try:
                     tree = etree.parse(urllib2.urlopen(full_url), htmlparser)
                 except urllib2.HTTPError:
@@ -120,10 +166,13 @@ class Finder(object):
                 for rule in self.rules:
                     if not rule.check(tree):
                         acc = False
+                        break
                 if acc:
                     content += full_url + '\n'
-            self.send_email(content)
+            self.sendmail(content)
+            self.processed_urls_file.flush()
             time.sleep(60)
+
 
 if __name__ == "__main__":
     Finder('config').run()
